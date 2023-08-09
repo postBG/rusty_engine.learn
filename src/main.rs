81ee06ad721d7fc62c808f9bb812ffc67db20a72
postBG <profile2697@gmail.com>
2023-08-10 03:22:39 +0900

    [31] tutorial: sprites

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,18 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation = Vec2::new(0.0, 0.0);
+    player.rotation = std::f32::consts::FRAC_PI_2;
+    player.scale = 0.5;
+    player.layer = 1.0;
+
+    let player2 = game.add_sprite("temporary", SpritePreset::RacingCarRed);
+    player2.translation = Vec2::new(30.0, 0.0);
+    player2.layer = 0.0;
+
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
-fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    game_state.current_score += 1;
-    println!("Current Score: {}", game_state.current_score);
-}
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {}
